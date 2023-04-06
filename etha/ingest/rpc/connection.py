a7e4e1b44d23ba9f4026d650c98b90605775ac20
tmcgroul <safonov.a.r@mail.ru>
2023-04-06 13:35:01 +0300

    extend retry cases

--- etha/ingest/rpc/connection.py
+++ etha/ingest/rpc/connection.py
@@ -222,5 +222,7 @@ def _is_retryable_error(e: Exception) -> bool:
         return e.response.status_code in (429, 502, 503, 504)
     elif isinstance(e, httpx.ConnectError) or isinstance(e, httpx.TimeoutException):
         return True
+    elif isinstance(e, RpcError) and isinstance(e.info, dict) and e.info.get('code') == 429:
+        return True
     else:
         return False
