f92aca0e9992b230f0971b1cbc48281c6061d483
Adam Wierzbicki <a.wierzbicki@subsquid.io>
2023-06-22 13:04:25 +0200

    A more elegant way to implement query profiling

--- etha/query/runner.py
+++ etha/query/runner.py
@@ -1,5 +1,4 @@
 import tempfile
-from typing import Optional
 
 import duckdb
 import pyarrow
@@ -8,7 +7,6 @@ from etha.fs import LocalFs
 from etha.layout import DataChunk, get_filelist
 from etha.query.model import Query
 from etha.query.sql import SqlQuery
-
 
 CON = duckdb.connect(':memory:')
 
@@ -19,19 +17,33 @@ class QueryRunner:
         self._query = SqlQuery(q, get_filelist(LocalFs(dataset_dir)))
         # print(self._query.sql)
 
-    def visit(self, chunk: DataChunk, profiling: bool = False) -> (pyarrow.ChunkedArray, Optional[str]):
+    def visit(self, chunk: DataChunk) -> pyarrow.ChunkedArray:
         self._query.set_chunk(self._dataset_dir, chunk)
+        CON.execute(self._query.sql, self._query.params)
+        return CON.fetch_arrow_table().column(0)
 
-        if profiling:
-            with tempfile.NamedTemporaryFile(mode='r', suffix='.json', delete=False) as profile_out:
-                CON.execute(f"PRAGMA enable_profiling='json'; PRAGMA profile_output='{profile_out.name}';")
-                CON.execute(self._query.sql, self._query.params)
-                result = CON.fetch_arrow_table().column(0)
+
+class ProfilingQueryRunner(QueryRunner):
+    def __init__(self, dataset_dir: str, q: Query):
+        super().__init__(dataset_dir=dataset_dir, q=q)
+        self._exec_plans = []
+
+    def visit(self, chunk: DataChunk) -> pyarrow.ChunkedArray:
+        with tempfile.NamedTemporaryFile(mode='r', suffix='.json') as profile_out:
+            CON.execute(f"PRAGMA enable_profiling='json'; PRAGMA profile_output='{profile_out.name}';")
+            try:
+                result = super().visit(chunk=chunk)
+                self._exec_plans.append(profile_out.read())
+                return result
+            finally:
                 CON.execute(f"PRAGMA disable_profiling;")
-                profile_json = profile_out.read()
-                return result, profile_json
-        else:
-            CON.execute(self._query.sql, self._query.params)
-            result = CON.fetch_arrow_table().column(0)
-            return result, None
 
+    @property
+    def exec_plans(self) -> [str]:
+        return self._exec_plans
+
+
+def get_query_runner(dataset_dir: str, q: Query, profiling: bool = False) -> QueryRunner:
+    if profiling:
+        return ProfilingQueryRunner(dataset_dir=dataset_dir, q=q)
+    return QueryRunner(dataset_dir=dataset_dir, q=q)
--- etha/worker/query.py
+++ etha/worker/query.py
@@ -5,7 +5,7 @@ from typing import Iterable, Optional, NamedTuple
 from etha.fs import LocalFs
 from etha.layout import get_chunks
 from etha.query.model import Query
-from etha.query.runner import QueryRunner
+from etha.query.runner import get_query_runner
 from etha.worker.state.intervals import Range
 
 
@@ -21,23 +21,20 @@ def execute_query(dataset_dir: str, data_range: Range, q: Query, profiling: bool
     assert first_block <= last_block
 
     num_read_chunks = 0
-    exec_plans = []
+    runner = get_query_runner(dataset_dir=dataset_dir, q=q, profiling=profiling)
 
     def json_lines() -> Iterable[str]:
         nonlocal num_read_chunks
         beg = datetime.datetime.now()
-        runner = QueryRunner(dataset_dir, q)
         size = 0
 
         for chunk in get_chunks(LocalFs(dataset_dir), first_block=first_block, last_block=last_block):
             try:
-                rows, exec_plan = runner.visit(chunk, profiling=profiling)
+                rows = runner.visit(chunk)
             except Exception as e:
                 e.add_note(f'data chunk: ${chunk.path()}')
                 raise e
 
-            if profiling:
-                exec_plans.append(exec_plan)
             num_read_chunks += 1
 
             for row in rows:
@@ -54,5 +51,5 @@ def execute_query(dataset_dir: str, data_range: Range, q: Query, profiling: bool
     return QueryResult(
         result=f'[{",".join(json_lines())}]',
         num_read_chunks=num_read_chunks,
-        exec_plan=f'[{",".join(exec_plans)}]' if profiling else None
+        exec_plan=f'[{",".join(runner.exec_plans)}]' if profiling else None
     )
