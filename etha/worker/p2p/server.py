aca6bf25b8e81178e86d150f22b5d567b5462795
Adam Wierzbicki <a.wierzbicki@subsquid.io>
2023-07-25 10:50:06 +0200

    Sentry integration for P2P worker

--- etha/worker/p2p/server.py
+++ etha/worker/p2p/server.py
@@ -292,4 +292,9 @@ async def _main():
 
 
 def main():
+    if os.getenv('SENTRY_DSN'):
+        import sentry_sdk
+        sentry_sdk.init(
+            traces_sample_rate=1.0
+        )
     run_async_program(_main, log=LOG)
