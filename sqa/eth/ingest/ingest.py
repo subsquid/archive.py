b83b83c1f4fca9b3e4a3f7f8ad86ad2bf4d2280c
Igor Mahov <tetya.a@yandex.ru>
2024-02-22 17:45:51 +0300

    fix: debug trace handle error

--- sqa/eth/ingest/ingest.py
+++ sqa/eth/ingest/ingest.py
@@ -482,8 +482,11 @@ def _fix_astar_995596(block: Block):
 
 def _validate_debug_trace(result):
     for trace in result:
-        if len(trace.keys()) == 1 and trace.get('error') == 'execution timeout':
-            return False
+        if trace.get('error') is not None:
+            if trace.get('error') == 'execution timeout':
+                return False
+            if 'nonce too high' in trace.get('error'):
+                return False
     return True
 
 
